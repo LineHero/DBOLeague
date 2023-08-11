39bed2702217c476c0680320b91de46e0a8f0b36
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2023-08-11 11:13:20 +0900

    프로젝트 생성

---
+++ DBOLeague/src/main/java/com/hero/DBOLeague/DboLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.hero.DBOLeague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DboLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DboLeagueApplication.class, args);
+	}
+
+}
