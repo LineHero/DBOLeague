ce18b8973b8d3e7cb90149f3f78a55c637c8c115
blood <blood@Sunwoong>
2023-08-14 17:25:26 +0900

    메인 이미지 추가

--- DBOLeague/src/main/java/controller/MainController.java
+++ DBOLeague/src/main/java/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	@RequestMapping("/main")
+	@RequestMapping("/maintest")
 	public String maintest() {
 		return "DBOMain";
 	}
