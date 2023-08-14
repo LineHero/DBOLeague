dc3e6c080691a9d28ff3c10afc5dd63416313452
MKLee <quler101@gmail.com>
2023-08-14 12:26:43 +0900

    pom dependency add, jquery file add, name changed

--- DBOLeague/src/main/java/controller/RankingController.java
+++ DBOLeague/src/main/java/controller/RankingController.java
@@ -3,14 +3,23 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import service.RankingService;
 
 @Controller
-public class AdminController {
+public class RankingController {
 	
 	@Autowired
 	@Qualifier("rankingServiceImpl")
 	RankingService service;
+	
+	@RequestMapping("/ranking")
+	public ModelAndView Ranking() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ranking");
+		return mv;
+	}
 	
 }
