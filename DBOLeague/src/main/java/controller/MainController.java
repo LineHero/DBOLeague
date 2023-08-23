6aee65a7ac181dc52263b0e944cb7b37ef092c06
blood <blood@Sunwoong>
2023-08-23 13:32:46 +0900

    마무리

--- DBOLeague/src/main/java/controller/MainController.java
+++ DBOLeague/src/main/java/controller/MainController.java
@@ -2,16 +2,20 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
-import jakarta.servlet.http.HttpSession;
+import dto.MemberDTO;
 
 @Controller
 public class MainController {
 	
 	@RequestMapping("/main")
-	public ModelAndView main(HttpSession session) {
+	public ModelAndView main(@SessionAttribute(name = "userlogin", required = false)MemberDTO dto) {
 		ModelAndView mv = new ModelAndView();
+		if (dto != null) {
+			mv.addObject("loginNick", dto.getMember_nick());
+		}
 		mv.setViewName("DBOMain");
 		return mv;
 	}
